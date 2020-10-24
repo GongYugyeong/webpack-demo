3113032768573b5917225f97990517afc5bb7348
GongYugyeong <yugyeong0917@naver.com>
2020-10-24 17:00:40 +0900

    second commit

---
+++ src/js/cube.js
@@ -0,0 +1,11 @@
+const volume = x => x * x * x;
+const area = x => x * x;
+// CommonJS 전체모듈화 module.exports = 전체 내용
+// => es 모듈에서는 export default 전체
+
+/*export default {
+  volume, // shorthand propery
+  area
+}*/
+
+export default volume
